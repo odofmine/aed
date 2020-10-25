de74457857578e5be7fc6a6e1005fa0f3b6d387f
littleee <littleee.lau@gmail.com>
2020-10-25 21:54:50 +0800

    update

--- autopush.js
+++ autopush.js
@@ -3,9 +3,10 @@ var execSync = require('child_process').execSync;
 const sleep = (ms) => new Promise(resove => setTimeout(resove, ms));
 
 async function main() {
-  execSync('git add .')
-  execSync(`git commit -m'update ${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}'`)
-  execSync('git push origin main')
+ // execSync('git add .')
+ // execSync(`git commit -m'update ${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}'`)
+ // execSync('git push origin main')
+console.log(1)
   sleep(300000)
 }
 
